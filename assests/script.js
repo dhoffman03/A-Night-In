f0bd383a382e92aa58fb35e7892ffdd72482435f
Mona Lisa <giatran1222@gmail.com>
2022-08-15 20:03:49 -0700

    html btns and headings

--- assests/script.js
+++ assests/script.js
@@ -6,6 +6,17 @@ var movieAPIKey = "k_4qje020e";
 var requestURL =
   "https://imdb-api.com/API/AdvancedSearch/k_4qje020e/?genres=action,adventure";
 
+function getInput() {
+    $("#movie-search").each(function() {
+        var input = $(this).text();
+        console.log(input)
+
+        if(getInput === "Select a Genre") {
+            console.log("select a genre")
+        };
+    });
+}
+getInput();
 
 fetch(requestURL, {
   method: "Get",
@@ -14,7 +25,17 @@ fetch(requestURL, {
 })
   .then(function (response) {
     return response.json();
+})
+  .then(function(data){
+    console.log(data);
 
+    var movieTitle = $(data.results[0].title);
+    console.log(movieTitle)
+    var movieImage = $(data.results.image);
+    var movieRating = $(data.results.contentRating);
+    var moviePlot = $(data.results.plot);
+    var movieGenres = $(data.results.genres);
+});
 
 //Reed script sandbox
 var drinkAPIKey = "5618241aea289752355d852d3165a903";
