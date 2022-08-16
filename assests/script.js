e64fac96803a45d777404cf88b55233ab6295174
Mona Lisa <giatran1222@gmail.com>
2022-08-15 18:15:06 -0700

    changed script order and some js

--- assests/script.js
+++ assests/script.js
@@ -5,9 +5,29 @@
 
 
 //Gia script sandbox
+var movieAPIKey = "k_4qje020e"
 
+var requestURL = "https://imdb-api.com/API/AdvancedSearch/k_4qje020e/?genres=action,adventure"
 
+function movieInput() {
+    $(".movie-search").function(); {
+        var input = $(this).children().val()
+        console.log(input)
+    }
+}
+movieInput ()
 
+fetch (requestURL, {
+    method: "Get",
+    credential: "same-origin",
+    redirect: "follow",
+})
+  .then(function(response){
+    return response.json();
+   })
+   .then (function(data){
+    console.log(data)
+   })
 
 
 
