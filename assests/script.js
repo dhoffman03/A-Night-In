74336590f0631304b9046fe26ed755afabe97923
Your Name <you@example.com>
2022-08-17 14:09:54 -0500

    added collapsable history panel

--- assests/script.js
+++ assests/script.js
@@ -1,49 +1,49 @@
-//for the tabs /RM
+// //for the tabs /RM
 
 
-//Dymond script sandbox
+// //Dymond script sandbox
 
-//Gia script sandbox
-var movieAPIKey = "k_4qje020e";
+// //Gia script sandbox
+// var movieAPIKey = "k_4qje020e";
 
-var requestURL =
-  "https://imdb-api.com/API/AdvancedSearch/k_4qje020e/?genres=action,adventure";
+// var requestURL =
+//   "https://imdb-api.com/API/AdvancedSearch/k_4qje020e/?genres=action,adventure";
 
-function getInput() {
-    $("#movie-search").each(function() {
-        var input = $(this).text();
-        console.log(input)
+// function getInput() {
+//     $("#movie-search").each(function() {
+//         var input = $(this).text();
+//         console.log(input)
 
-        if(getInput === "Select a Genre") {
-            console.log("select a genre")
-        };
-    });
-}
-getInput();
+//         if(getInput === "Select a Genre") {
+//             console.log("select a genre")
+//         };
+//     });
+// }
+// getInput();
 
-fetch(requestURL, {
-  method: "Get",
-  credential: "same-origin",
-  redirect: "follow",
-})
-  .then(function (response) {
-    return response.json();
+// fetch(requestURL, {
+//   method: "Get",
+//   credential: "same-origin",
+//   redirect: "follow",
+// })
+//   .then(function (response) {
+//     return response.json();
 
-})
-  .then(function(data){
-    console.log(data.results);
+// })
+//   .then(function(data){
+//     console.log(data.results);
 
-    var movieTitle = data.results[0].title;
-    console.log(movieTitle)
-    var movieImage = data.results[0].image;
-    console.log(movieImage)
-    var movieRating = data.results[0].contentRating;
-    console.log(movieRating)
-    var moviePlot = data.results[0].plot;
-    console.log(moviePlot)
-    var movieGenres = data.results[0].genres;
-    console.log(movieGenres)
-});
+//     var movieTitle = data.results[0].title;
+//     console.log(movieTitle)
+//     var movieImage = data.results[0].image;
+//     console.log(movieImage)
+//     var movieRating = data.results[0].contentRating;
+//     console.log(movieRating)
+//     var moviePlot = data.results[0].plot;
+//     console.log(moviePlot)
+//     var movieGenres = data.results[0].genres;
+//     console.log(movieGenres)
+// });
 
 //Reed script sandbox
 var drinkAPIKey = "5618241aea289752355d852d3165a903";
@@ -70,50 +70,55 @@ fetch(querydrinkAPI).then(function (response) {
       console.log(data);
     });
   }
-}); //RM added this
+}); 
 
-//for handling submission of the drink form
-// var drinkFormSubmitHandler = function (event) {
-//   event.preventDefault();
+// for handling submission of the drink form
+var drinkFormSubmitHandler = function (event) {
+  event.preventDefault();
 
-//   //to get the user input data
-//   var userDrinkInput = drinkSearchInputEl.value.trim();
-//   var userDrinkStyleInput = drinkStyleSearchEl.value.trim();
-//   //to get the API
-//   var querydrinkAPI =
-//     "https://api.edamam.com/api/recipes/v2?type=public&q=" +
-//     userDrinkInput +
-//     "&app_id=f77c6a8d&app_key=5618241aea289752355d852d3165a903&health=" +
-//     userDrinkStyleInput +
-//     "&imageSize=REGULAR";
+  //to get the user input data
+  var userDrinkInput = drinkSearchInputEl.value.trim();
+  var userDrinkStyleInput = drinkStyleSearchEl.value.trim();
+  //to get the API
+  var querydrinkAPI =
+    "https://api.edamam.com/api/recipes/v2?type=public&q=" +
+    userDrinkInput +
+    "&app_id=f77c6a8d&app_key=5618241aea289752355d852d3165a903&health=" +
+    userDrinkStyleInput +
+    "&imageSize=REGULAR";
 
-//   if (userDrinkInput || userDrinkStyleInput) {
-//     fetch(querydrinkAPI).then(function (response) {
-//       if (response.ok) {
-//         response.json().then(function (data) {
-//           //check how the process is going
-//           console.log(data);
+  if (userDrinkInput && userDrinkStyleInput) {
+    fetch(querydrinkAPI).then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          //check how the process is going
+          console.log(data);
+          console.log(userDrinkInput);
+          console.log(userDrinkStyleInput);
+          //here are the objetcts we need for the site
+          var recipeName = data.hits[0].recipe.label;
+          //ing. list,images and steps will need functions to get at all the objects within
+          var ingredientsList = data.hits[0].recipe.ingredientLines;
+          var recipeSteps = data.hits[0].recipe.ingredients;
+          var recipeSource = data.hits[0].recipe.source;
+          var recipeImage = data.hits[0].recipe.images;
+            // var recipeLink = $(data.hits[0].recipe.url); this line isn't working for some reason..
+          var drinkPrint = $("#drink-recipe-print");
+          console.log(recipeName);
+          //display the recipe
+          $("#drink-title").text(recipeName);
+          // drinkPrint.text(ingredientsList);
+          // drinkPrint.text(recipeSteps);
+          // drinkPrint.text(recipeSource);
 
-//           //here are the objetcts we need for the site
-//           var recipeName = $(data.hits[0].recipe.label);
-//           //ing. list,images and steps will need functions to get at all the objects within
-//           var ingredientsList = $(data.hits[0].recipe.ingredientLines);
-//           var recipeSteps = $(data.hits[0].recipe.ingredients);
-//           var recipeSource = $(data.hits[0].recipe.source);
-//           var recipeImage = $(data.hits[0].recipe.images);
-//             // var recipeLink = $(data.hits[0].recipe.url); this line isn't working for some reason..
-//           var drinkPrint = $("#drink-recipe-print");
+        });
+      }
+    });
+  }
+};
 
-//           //display the recipe
-//           drinkPrint.text(recipeName);
-//           drinkPrint.text(ingredientsList);
-//           drinkPrint.text(recipeSteps);
-//           drinkPrint.text(recipeSource);
+//need functions to split and print the ingredients and recipes steps (those are string arrays)
 
-//         });
-//       }
-//     });
-//   }
-// };
+//need local history 
 
-// drinkFormEl.addEventListener("submit", drinkFormSubmitHandler);
+drinkFormEl.addEventListener("submit", drinkFormSubmitHandler);
