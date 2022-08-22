9d0644020062a4f5f3e02e13307b179a64d77e82
Dymond <dymond.hoffman03@gmail.com>
2022-08-21 23:55:28 -0700

    meal form complete, food recipes go to local storage

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,8 +1,19 @@
+//Dymond script sandbox
 //Declare global vars
-var foodApiKey = "&app_key=d20058a43c19a20e533d6b06c2c26156";
-var foodAppId = "&app_id=9fc7bc31";
+
+var foodApiKey = "&app_key=9e715a0764f4ea504885da5433e5c920";
+var foodAppId = "&app_id=69d122e0";
 var foodFormEl = $("#food-form")
 
+//Set arrays for local storage of searched and saved meals
+var mealRecipeSearchHistory = JSON.parse(localStorage.getItem("Dinner recipe search"));
+if (!mealRecipeSearchHistory) {
+  mealRecipeSearchHistory = [];
+}
+var savedMeals = JSON.parse(localStorage.getItem("saved meals"));
+if (!savedMeals) {
+  savedMeals = [];
+}
 //Handle submissions from drink form
 function handleFoodSearchSubmit() {
 
@@ -20,8 +31,6 @@ function handleFoodSearchSubmit() {
   fetch(foodURL).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        console.log(data.hits[0].recipe);
-        console.log(userFoodType);
 
         //Declare local vars from api recipe data
         var foodImg = data.hits[0].recipe.images.SMALL.url;
@@ -35,12 +44,9 @@ function handleFoodSearchSubmit() {
         //Creat <a> and link recipe url
         let fullRecipe = "<a class='drink-ext-link' href=" + foodLink + ">Click here for complete directions.</a>";
 
-        //Create a save btn
-        let foodSave = "<button class='save-recipe rounded-md p-2 mt-2' id='food-save-btn;'>Save Recipe</button>";
-
         //make recipe.ingredientsLines array into <li>
         for (var i = 0; i < ingList.length; i++) {
-          foodIngList += "<li>" + ingList[i] + "</li><br>";
+          foodIngList += "<li class=food-ing-list>" + ingList[i] + "</li><br>";
 
         } foodIngList += "</ul>";
 
@@ -50,19 +56,83 @@ function handleFoodSearchSubmit() {
         $("#food-list").html(foodIngList);
         $("#food-source").text("Recipe from " + foodSource + ".");
         $("#food-link").html(fullRecipe);
-        $("#food-save").html(foodSave)
+        //Make save button visibe when user submits form
+        $("#food-save").removeClass("invisible")
+        $("#food-save").addClass("visible")
 
+        const mealData = {
+          mealTitle: data.hits[0].recipe.label,
+          mealImage: data.hits[0].recipe.images.SMALL.url,
+          mealIngList: data.hits[0].recipe.ingredientLines,
+          mealSource: data.hits[0].recipe.source,
+          mealLink: data.hits[0].recipe.url,
+        }
+
+        //Add all saved search recipes to local storage
+        mealRecipeSearchHistory.unshift(mealData);
+        localStorage.setItem("Dinner recipe search", JSON.stringify(mealRecipeSearchHistory));
       })
     }
   })
 }
 
+//Local storage functions
+function storeMealRecipe() {
+
+  const savedMeal = {
+    mealTitle: mealRecipeSearchHistory[0].mealTitle,
+    mealImage: mealRecipeSearchHistory[0].mealImage,
+    mealIngList: mealRecipeSearchHistory[0].mealIngList,
+    mealSource: mealRecipeSearchHistory[0].mealSource,
+    mealLink: mealRecipeSearchHistory[0].mealLink,
+  }
+
+  //Add saved recipes to local storage
+  savedMeals.unshift(savedMeal);
+  localStorage.setItem("saved meals", JSON.stringify(savedMeals));
+
+
+  var savedRecipe = document.createElement("a");
+  savedRecipe.id = "savedMeal";
+  //When saved recipe is clicked, user is directed to the recipe's website
+  savedRecipe.href = savedMeals[0].mealLink;
+  savedRecipe.innerHTML = savedMeals[0].mealTitle;
+
+  //Append recipe to sidebar
+  $("#savedMealRecipes").append(savedRecipe)
+}
+
+//append all recipes in local storage
+function addMealToSaved() {
+  for (var i = 0; i < savedMeals.length; i++) {
+
+    var savedRecipe = document.createElement("a");
+    savedRecipe.id = "savedMeal";
+    savedRecipe.href = savedMeals[i].mealLink;
+    savedRecipe.innerHTML = savedMeals[i].mealTitle;
+
+    $("#savedMealRecipes").append(savedRecipe)
+  }
+}
+
 //Run function when form is submited
-foodFormEl.on("submit", function (event) {
-  event.preventDefault();
+foodFormEl.on("submit", function (e) {
+  e.preventDefault();
 
   handleFoodSearchSubmit();
+
 });
+
+//Run local storage function when 'save recipe' button is clicked
+$($("#food-save")).on("click", function (e) {
+  e.preventDefault();
+
+  storeMealRecipe();
+});
+
+//Run function to append saved recipes to sidebar
+addMealToSaved()
+
 
 //Gia script sandbox
 var movieAPIKey = "k_4qje020e";
