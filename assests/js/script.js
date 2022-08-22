8001bbce137fd3fc1894b197b1fb57337c724dae
Mona Lisa <giatran1222@gmail.com>
2022-08-22 14:11:27 -0700

    re-added Dymond's js b/c my push accidentally got rid of it

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,42 +1,141 @@
-// //for the tabs /RM
-
 //Dymond script sandbox
-var foodApiKey = "&app_key=d20058a43c19a20e533d6b06c2c26156";
-var foodAppId = "&app_id=9fc7bc31";
-var foodFormEl = $("#food-form");
+//Declare global vars
 
+var foodApiKey = "&app_key=9e715a0764f4ea504885da5433e5c920";
+var foodAppId = "&app_id=69d122e0";
+var foodFormEl = $("#food-form")
+
+//Set arrays for local storage of searched and saved meals
+var mealRecipeSearchHistory = JSON.parse(localStorage.getItem("Dinner recipe search"));
+if (!mealRecipeSearchHistory) {
+  mealRecipeSearchHistory = [];
+}
+var savedMeals = JSON.parse(localStorage.getItem("saved meals"));
+if (!savedMeals) {
+  savedMeals = [];
+}
 //Handle submissions from drink form
 function handleFoodSearchSubmit() {
+
   var userFoodType = $("#food-search").val();
 
   //Declare edamamAPI url
   var foodURL =
-    "https://api.edamam.com/api/recipes/v2?type=public&q=" +
-    foodAppId +
+    "https://api.edamam.com/api/recipes/v2?type=public&q=" + foodAppId +
     foodApiKey +
-    "&health=alcohol-free&cuisineType=" +
-    userFoodType +
-    "&mealType=Dinner&random=true";
+    "&cuisineType=" + userFoodType
+    + "&mealType=Dinner&random=true";
+
 
   //Request api url
   fetch(foodURL).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        console.log(data.hits[0].recipe);
-        console.log(userFoodType);
-      });
+
+        //Declare local vars from api recipe data
+        var foodImg = data.hits[0].recipe.images.SMALL.url;
+        var recipeLabel = data.hits[0].recipe.label;
+        var ingList = data.hits[0].recipe.ingredientLines;
+        var foodSource = data.hits[0].recipe.source;
+        var foodLink = data.hits[0].recipe.url;
+
+        let foodIngList = "<ul>";
+
+        //Creat <a> and link recipe url
+        let fullRecipe = "<a class='drink-ext-link' href=" + foodLink + ">Click here for complete directions.</a>";
+
+        //make recipe.ingredientsLines array into <li>
+        for (var i = 0; i < ingList.length; i++) {
+          foodIngList += "<li class=food-ing-list>" + ingList[i] + "</li><br>";
+
+        } foodIngList += "</ul>";
+
+        //Display results on page
+        $("#food-img").html($("#food-img").attr("src", foodImg));
+        $("#food-label").text(recipeLabel);
+        $("#food-list").html(foodIngList);
+        $("#food-source").text("Recipe from " + foodSource + ".");
+        $("#food-link").html(fullRecipe);
+        //Make save button visibe when user submits form
+        $("#food-save").removeClass("invisible")
+        $("#food-save").addClass("visible")
+
+        const mealData = {
+          mealTitle: data.hits[0].recipe.label,
+          mealImage: data.hits[0].recipe.images.SMALL.url,
+          mealIngList: data.hits[0].recipe.ingredientLines,
+          mealSource: data.hits[0].recipe.source,
+          mealLink: data.hits[0].recipe.url,
+        }
+
+        //Add all saved search recipes to local storage
+        mealRecipeSearchHistory.unshift(mealData);
+        localStorage.setItem("Dinner recipe search", JSON.stringify(mealRecipeSearchHistory));
+      })
     }
-  });
+  })
+}
+
+//Local storage functions
+function storeMealRecipe() {
+
+  const savedMeal = {
+    mealTitle: mealRecipeSearchHistory[0].mealTitle,
+    mealImage: mealRecipeSearchHistory[0].mealImage,
+    mealIngList: mealRecipeSearchHistory[0].mealIngList,
+    mealSource: mealRecipeSearchHistory[0].mealSource,
+    mealLink: mealRecipeSearchHistory[0].mealLink,
+  }
+
+  //Add saved recipes to local storage
+  savedMeals.unshift(savedMeal);
+  localStorage.setItem("saved meals", JSON.stringify(savedMeals));
+
+
+  var savedRecipe = document.createElement("a");
+  savedRecipe.id = "savedMeal";
+  //When saved recipe is clicked, user is directed to the recipe's website
+  savedRecipe.href = savedMeals[0].mealLink;
+  savedRecipe.innerHTML = savedMeals[0].mealTitle;
+
+  //Append recipe to sidebar
+  $("#savedMealRecipes").append(savedRecipe)
+}
+
+//append all recipes in local storage
+function addMealToSaved() {
+  for (var i = 0; i < savedMeals.length; i++) {
+
+    var savedRecipe = document.createElement("a");
+    savedRecipe.id = "savedMeal";
+    savedRecipe.href = savedMeals[i].mealLink;
+    savedRecipe.innerHTML = savedMeals[i].mealTitle;
+
+    $("#savedMealRecipes").append(savedRecipe)
+  }
 }
 
 //Run function when form is submited
-foodFormEl.on("submit", function (event) {
-  event.preventDefault();
+foodFormEl.on("submit", function (e) {
+  e.preventDefault();
 
   handleFoodSearchSubmit();
+
 });
 
+//Run local storage function when 'save recipe' button is clicked
+$($("#food-save")).on("click", function (e) {
+  e.preventDefault();
+
+  storeMealRecipe();
+});
+
+//Run function to append saved recipes to sidebar
+addMealToSaved()
+
+
 //Gia script sandbox
+//global variables
 var movieAPIKey = "k_4qje020e";
 var movieContentEl = document.querySelector("#movie-results-print");
 var movieForm = document.getElementById("movie-form");
@@ -118,7 +217,7 @@ function searchMovieAPI(genreInputVal) {
       });
 
       data.results.splice(50);
-      //looping through to pull all movie data I need
+      //looping through all movie data and pulling 5 movies
       for (var i = 0; i < 5; i++) {
         // console.log(data.results[i]);
         displayMovie(data.results[i]);
